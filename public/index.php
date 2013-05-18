8e5773619293964fd873399d710ce8d309ec214c
Tom Green <kingofcuddles@gmail.com>
2013-05-18 12:15:16 +0100

    Cleaned up the index file.

--- public/index.php
+++ public/index.php
@@ -5,20 +5,9 @@
 	$battleTag = 'BTG#2577';
 	$args = ['protocol' => 'http://', 'server' => 'eu', 'locale' => 'en_GB'];
 
-	$D3 = new D3 ($battleTag);
+	$D3 = new D3 ($args);
 
-/*
+	// Career Data
+	var_dump($D3->getCareer($battleTag));
 
-	$url = 'http://eu.battle.net/api/d3/profile/BTG-2577/index?locale=en_GB';
-	$ch = curl_init();
-	$timeout = 5;
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-	$data = curl_exec($ch);
-	curl_close($ch);
-
-	echo '<pre>';
-	print_r($data);
-	echo '</pre>';*/
 ?>
